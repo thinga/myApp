9c6af98241ac2303c6b2c141bf543f0c7b15fd17
thinga <32902065+thinga@users.noreply.github.com>
2021-07-01 23:02:45 +0200

    41. Adding a token service

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -39,6 +41,7 @@ namespace API
 
             services.AddControllers();
             services.AddCors();
+            services.AddScoped<ITokenService, TokenService>();
            
         }
 
