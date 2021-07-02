090994a0db2bcf84ad33b18dc495d5827a57235d
thinga <32902065+thinga@users.noreply.github.com>
2021-07-02 16:56:47 +0200

    49. Introduction to Angular template forms

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule} from '@angular/common/http';      
+import { HttpClientModule} from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';      
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
