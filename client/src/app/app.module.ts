6f3f02c49d6809b65f69b52bf63e5a7264920b8f
thinga <32902065+thinga@users.noreply.github.com>
2021-07-09 15:49:29 +0200

    67. Adding a toast service for notifications

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MembersListComponent } from './members-list/members-list.component';
 import { MemberDetailComponent } from './member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   exports: [CommonModule],
@@ -36,7 +37,10 @@ import { MessagesComponent } from './messages/messages.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right' })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -10,24 +11,25 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
 model: any = {};
 
-constructor(public accountService: AccountService, private router: Router ) { }
+constructor(public accountService: AccountService, private router: Router,
+  private toastr: ToastrService) { }
 
-ngOnInit(): void {
-
-}
+ngOnInit(): void {  }
 
 login(){
-  this.accountService.login(this.model).subscribe(response => {
-    this.router.navigateByUrl('/members');
-  }, error => {
-    console.log(error);
-  });
- }
+this.accountService.login(this.model).subscribe(response => {
+this.router.navigateByUrl('/members');
+}, error => {
+console.log(error);
+this.toastr.error(error.error);
+});
+}
 
 logout(){
-  this.accountService.logout();
-  this.router.navigateByUrl('/');
+this.accountService.logout();
+this.router.navigateByUrl('/');
 }
 }
+
 
 
