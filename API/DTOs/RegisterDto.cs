f35a252f7e6ff3d1c6f22a7e8d4db8207528816c
thinga <32902065+thinga@users.noreply.github.com>
2021-07-14 16:12:17 +0200

    74. Creating an error controller for testing errors

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -4,9 +4,12 @@ namespace API.DTOs
 {
     public class RegisterDto
     {
-        [Required] 
-         public string Username { get; set;}
-         [Required] 
+         [Required]
+        public string Username { get; set;}
+
+        [Required]
+        [StringLength(8, MinimumLength = 4)]
+    
         public string Password { get; set; }
 
     }
