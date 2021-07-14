eb618f229ec6b676a051a21e5a2ef4398ee40cc5
thinga <32902065+thinga@users.noreply.github.com>
2021-07-14 15:59:47 +0200

    71. Tidying up the app module by using a shared module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,7 +16,8 @@ import { MembersListComponent } from './members/members-list/members-list.compon
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
+
 
 @NgModule({
   exports: [CommonModule],
@@ -37,9 +38,7 @@ import { ToastrModule } from 'ngx-toastr';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot(),
-    ToastrModule.forRoot({
-      positionClass: 'toast-bottom-right' })
+    SharedModule
 
   ],
   providers: [],
