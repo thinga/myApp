76cbf69303456940bdea4af1056e5d923f25d54e
thinga <32902065+thinga@users.noreply.github.com>
2021-08-18 10:16:53 +0200

    8/96. Configuring AutoMapper

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using API.DTOs;
 using API.Entities;
 using AutoMapper;
@@ -6,11 +7,14 @@ namespace API.Helpers
 {
     public class AutoMapperProfiles : Profile
     {
-         public AutoMapperProfiles()
+        public AutoMapperProfiles()
         {
-            CreateMap<AppUser, MemberDto>();
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src =>
+                src.Photos.FirstOrDefault(x => x.IsMain).Url));
             CreateMap<Photo, PhotoDto>();
-        } 
+        }
+
 
     }
 }
