573e2a759d483e5d8e9c45cf6b7075043ed34c5c
thinga <32902065+thinga@users.noreply.github.com>
2021-08-24 10:15:36 +0200

    11/127. Updating the users controller

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -16,6 +16,7 @@ namespace API.Extensions
 	        {
 				services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
 	            services.AddScoped<ITokenService, TokenService>();
+				services.AddScoped<IPhotoService, PhotoService>();
 			    services.AddScoped<IUserRepository, UserRepository>();
 				services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
 	            services.AddDbContext<DataContext>(options =>
@@ -30,7 +31,4 @@ namespace API.Extensions
 
     }
 
-    internal class CloudinarySettings
-    {
-    }
 }
