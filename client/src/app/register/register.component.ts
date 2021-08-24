2dc3b9c6a0f5d113edd6915540ba0353287d6bab
thinga <32902065+thinga@users.noreply.github.com>
2021-08-24 22:29:18 +0200

    12/147. Adding a reusable date input

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -13,11 +13,15 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
   registerForm: FormGroup;
+  maxDate: Date;
+
   constructor(private accountService: AccountService, private toastr: ToastrService,
      private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.intitializeForm();
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() -18);
   }
 
   intitializeForm() {
