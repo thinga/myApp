de1b8d2aac752b33b0ef8804ec5a20a4d4521c00
thinga <32902065+thinga@users.noreply.github.com>
2021-08-25 10:27:07 +0200

    12/149. Client side registration

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -71,11 +71,12 @@ export class PhotoEditorComponent implements OnInit {
       if (response) {
         const photo = JSON.parse(response);
         this.member.photos.push(photo);
+        if (photo.isMain) {
+          this.user.photoUrl = photo.url;
+          this.accountService.setCurrentUser(this.user);
+        }
 
-      }
-    }
-
-    
+      }  
   }
-
+  }
 }
