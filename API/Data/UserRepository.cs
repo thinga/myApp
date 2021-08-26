cc3e9ee55032d0123b9905836ccb2e6483c02976
thinga <32902065+thinga@users.noreply.github.com>
2021-08-26 11:15:55 +0200

    13/161. Adding sorting functionality

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -41,6 +41,13 @@ namespace API.Data
                      var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
                      var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
                      query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+                     
+                 query = userParams.OrderBy switch
+                  {
+                      "created" => query.OrderByDescending(u => u.Created),
+                      _ => query.OrderByDescending(u => u.LastActive)
+                  };                 
+
 
                      
               return await PagedList<MemberDto>.CreateAsync(query.ProjectTo<MemberDto>(_mapper
