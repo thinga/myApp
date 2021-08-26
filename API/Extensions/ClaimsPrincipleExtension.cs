2ab9e63651af4b6de7719276e4e7a7aad8fea5a2
thinga <32902065+thinga@users.noreply.github.com>
2021-08-26 14:03:12 +0200

    13/163. Making the Last Active action filter more optimal

--- API/Extensions/ClaimsPrincipleExtension.cs
+++ API/Extensions/ClaimsPrincipleExtension.cs
@@ -9,5 +9,11 @@ namespace API.Extensions
             return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         }
 
+            public static int GetUserId(this ClaimsPrincipal user)
+        {
+            return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+        }
+
+
     }
 }
