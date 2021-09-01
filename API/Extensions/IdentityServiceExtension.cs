c86a940ee08c41533cc2f6926f74373750c835c1
thinga <32902065+thinga@users.noreply.github.com>
2021-09-01 15:34:31 +0200

    16/208. Adding policy based authorisation

--- API/Extensions/IdentityServiceExtension.cs
+++ API/Extensions/IdentityServiceExtension.cs
@@ -37,9 +37,15 @@ namespace API.Extensions
 
                   };
               });
-              return services;
+
+                  services.AddAuthorization(opt => 
+               {
+                   opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                   opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+               });
+                     return services;
+        }
 
           }
         
     }
-}
