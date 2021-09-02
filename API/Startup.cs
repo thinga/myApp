661297439a23465b0b74c5816884ba039ba7e86e
thinga <32902065+thinga@users.noreply.github.com>
2021-09-02 11:00:18 +0200

    17/221. Authenticating to SignalR

--- API/Startup.cs
+++ API/Startup.cs
@@ -52,7 +52,10 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            app.UseCors(x => x.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .WithOrigins("https://localhost:4200"));
             app.UseAuthentication();
             app.UseAuthorization();
 
