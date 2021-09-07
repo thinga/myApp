085763a9e2d2fed86a5519215cedc910c338e0ec
thinga <32902065+thinga@users.noreply.github.com>
2021-09-07 17:50:30 +0200

    17/224. Displaying online presence

--- API/SignaIR/PresenceHub.cs
+++ API/SignaIR/PresenceHub.cs
@@ -7,14 +7,29 @@ namespace API.SignaIR
 {
     public class PresenceHub : Hub
     {
-            public override async Task OnConnectedAsync()
+        private readonly PresenceTracker _tracker;
+        public PresenceHub(PresenceTracker tracker)
         {
+            _tracker = tracker;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await _tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
             await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
+
+            var currentUsers = await _tracker.GetOnlineUsers();
+            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
+            await _tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
             await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+
+            await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+            await base.OnDisconnectedAsync(exception);
+
             await base.OnDisconnectedAsync(exception);
         }
 
