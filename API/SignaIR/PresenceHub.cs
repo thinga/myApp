8b0aaa69bac829c4bf2e5de19e1702b892b0c189
thinga <32902065+thinga@users.noreply.github.com>
2021-09-09 14:36:29 +0200

    17/234. Optimizing the presence

--- API/SignaIR/PresenceHub.cs
+++ API/SignaIR/PresenceHub.cs
@@ -15,19 +15,22 @@ namespace API.SignaIR
 
         public override async Task OnConnectedAsync()
         {
-            await _tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
+           var isOnline = await _tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
+           if (isOnline)
             await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
 
             var currentUsers = await _tracker.GetOnlineUsers();
-            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
+            await Clients.Caller.SendAsync("GetOnlineUsers", currentUsers);
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
-            await _tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
-            await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+           var isOffline = await _tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
 
-            await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+           if (isOffline)
+               await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+
+           
             await base.OnDisconnectedAsync(exception);
 
         }
