f9fcafd242534fdf39db9d328401cef102220f3a
thinga <32902065+thinga@users.noreply.github.com>
2021-09-10 11:40:17 +0200

    18/238. Implementing the unit of work

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -19,7 +19,7 @@ namespace API.Extensions
 				services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
 	            services.AddScoped<ITokenService, TokenService>();
 				services.AddScoped<IPhotoService, PhotoService>();
-			    services.AddScoped<IUserRepository, UserRepository>();
+			    services.AddScoped<IUnitOfWork, UnitOfWork>();
 				services.AddScoped<LogUserActivity>();
 				services.AddScoped<ILikesRepository, LikesRepository>();
 				services.AddScoped<IMessageRepository, MessageRepository>();
