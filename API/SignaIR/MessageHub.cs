5cd7276492797d19757495c50b5cb14a1d043545
thinga <32902065+thinga@users.noreply.github.com>
2021-09-10 10:34:35 +0200

    17/235. Optimizing the messages

--- API/SignaIR/MessageHub.cs
+++ API/SignaIR/MessageHub.cs
@@ -35,17 +35,22 @@ namespace API.SignaIR
         var otherUser = httpContext.Request.Query["user"].ToString();
         var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
         await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
-        await AddToGroup(Context, groupName);
+        
+        var group = await AddToGroup(groupName);
+       await Clients.Group(groupName).SendAsync("UpdatedGroup", group);
+        var messages = await _messageRepository.
+            GetMessageThread(Context.User.GetUsername(), otherUser);
 
-        var messages = await _messageRepository.GetMessageThread(Context.User.GetUsername(), otherUser);
+        await Clients.Caller.SendAsync("ReceiveMessageThread", messages);
 
-        await Clients.Group(groupName).SendAsync("ReceiveMessageThread", messages);
     }
 
     public override async Task OnDisconnectedAsync(Exception exception)
     {
-        await RemoveromMessageGroup(Context.ConnectionId);
+         var group = await RemoveFromMessageGroup();
+        await Clients.Group(group.Name).SendAsync("UpdatedGroup", group);
         await base.OnDisconnectedAsync(exception);
+
     }
 
     public async Task SendMessage(CreateMessageDto createMessageDto)
@@ -99,7 +104,7 @@ namespace API.SignaIR
 
     }
 
-    private async Task<bool> AddToGroup(HubCallerContext context, string groupName)
+    private async Task<Group> AddToGroup(string groupName)
     {
         var group = await _messageRepository.GetMessageGroup(groupName);
         var connection = new Connection(Context.ConnectionId, Context.User.GetUsername());
@@ -112,15 +117,23 @@ namespace API.SignaIR
 
         group.Connections.Add(connection);
 
-        return await _messageRepository.SaveAllAsync();
+        if (await _messageRepository.SaveAllAsync()) return group;
+
+        throw new HubException("Failed to join group");
 
     }
 
-    private async Task RemoveromMessageGroup(string connectionId)
+    private async Task<Group> RemoveFromMessageGroup()
     {
-        var connection = await _messageRepository.GetConnection(connectionId);
+       var group = await _messageRepository.GetGroupForConnection(Context.ConnectionId);
+        var connection = group.Connections.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
         _messageRepository.RemoveConnection(connection);
-        await _messageRepository.SaveAllAsync();
+
+         if (await _messageRepository.SaveAllAsync()) return group;
+
+         throw new HubException("Failed to remove from group");
+
+
     }
 
     private string GetGroupName(string caller, string other)
--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -4,6 +4,7 @@ import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { BehaviorSubject } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { Group } from '../_models/group';
 import { Message } from '../_models/message';
 import { User } from '../_models/User';
 import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
@@ -40,6 +41,21 @@ export class MessageService {
               this.messageThreadSource.next([...messages, message ])
          })
         })
+        
+        this.hubConnection.on('UpdatedGroup', (group: Group) => {
+          if (group.connections.some(x => x.username == otherUsername)) {
+            this.messageThread$.pipe(take(1)).subscribe(messages => {
+              messages.forEach(message => {
+                if (!message.dateRead) {
+                  message.dateRead = new Date(Date.now())
+                }
+              })
+              this.messageThreadSource.next([...messages]);
+            })
+          }
+        })
+
+
   }
 
   stopHubConnection() {
