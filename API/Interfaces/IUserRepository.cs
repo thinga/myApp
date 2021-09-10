ffb8ebf1086c5dc412e034dc4bb4e36cced3a507
thinga <32902065+thinga@users.noreply.github.com>
2021-09-10 21:52:39 +0200

    18/242. Optimizing queries part two

--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -15,5 +15,7 @@ namespace API.Interfaces
          Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams);
          Task<MemberDto> GetMemberAsync(string username );
 
+         Task<string> GetUserGender(string username);
+
     }
 }
